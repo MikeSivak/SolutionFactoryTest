9c64ace605faee20133224740332d5110a39d0c1
MikeSivak <noizemcnorm@gmail.com>
2023-05-02 14:16:48 +0300

    fixed cron job

--- src/modules/cronjobs/cronjobs.module.ts
+++ src/modules/cronjobs/cronjobs.module.ts
@@ -2,9 +2,10 @@ import { Module, forwardRef } from '@nestjs/common';
 import { CronjobsService } from './cronjobs.service';
 import { BoxesModule } from '../boxes/boxes.module';
 import { VisitsModule } from '../visits/visits.module';
+import { boxesProvider } from '../boxes/boxes.providers';
 
 @Module({
-  providers: [CronjobsService],
+  providers: [CronjobsService, ...boxesProvider],
   imports: [
     forwardRef(() => BoxesModule),
     forwardRef(() => VisitsModule),
--- src/modules/cronjobs/cronjobs.service.ts
+++ src/modules/cronjobs/cronjobs.service.ts
@@ -1,27 +1,28 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
-import { BoxesService } from '../boxes/boxes.service';
 import { Box } from '../boxes/box.entity';
-import { VisitsService } from '../visits/visits.service';
-import { Visit } from '../visits/visit.enity';
+import { BOX_REPOSITORY } from 'src/core/constants';
+import { Op } from 'sequelize';
 
 @Injectable()
 export class CronjobsService {
     constructor(
-        @Inject(BoxesService)
-        private readonly boxesService: BoxesService,
-        @Inject(VisitsService)
-        private readonly visitsService: VisitsService,
+        @Inject(BOX_REPOSITORY)
+        private readonly boxesRepository: typeof Box,
     ) { }
 
     @Cron('*/5 * * * * *')
-    async openForBusiness() {
-        const visits: Visit[] = await this.visitsService.findAll();
-        visits.map(async (visit) => {
-            const diffTime = Math.floor((((new Date()).getTime() - visit.createdAt.getTime()) / 1000 / 60) % 60);
-            if (diffTime >= 60) {
-                await this.boxesService.update(visit.boxId, { state: true });
-            }
-        });
+    async updateBoxStates() {
+        const now = new Date().getTime();
+        const bookingTime = now - 60 * 60 * 1000;
+        const isoDate = new Date(new Date(bookingTime).toISOString());
+
+        await this.boxesRepository.update({ state: true }, {
+            where: {
+                updatedAt: {
+                    [Op.lte]: isoDate,
+                },
+            },
+        })
     }
 }
